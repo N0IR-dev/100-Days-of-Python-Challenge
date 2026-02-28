0c6bde96a0ebc275fbb032b888e8ca0f01773fd6
NØIR <the.bloggers.hut@gmail.com>
2026-02-28 08:46:51 +0500

    Day 3
    This day features an even or odd number checker by seeing if the number is divisible by 2 with no remainder.

---
+++ day3.py
@@ -0,0 +1,9 @@
+# THIS IS DAY 3 OF THE 100 DAYS OF CODE CHALLENGE
+
+# --Even or Odd Number Checker--
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
