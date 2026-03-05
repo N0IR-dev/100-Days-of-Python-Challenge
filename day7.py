4838a87b36fd219366808eb4890f3058838f41a0
NØIR <the.bloggers.hut@gmail.com>
2026-03-05 06:14:05 +0500

    Day 7
    Day 7 features a Sentence Word Counter.

---
+++ day7.py
@@ -0,0 +1,5 @@
+#THIS IS DAY 7 OF THE 100 DAYS OF CODE CHALLENGE
+# --Word Counter in a sentence--
+sentence = input("Enter a sentence:")
+word_count = len(sentence.split())
+print(f"The number of words in the sentence is: {word_count}")
